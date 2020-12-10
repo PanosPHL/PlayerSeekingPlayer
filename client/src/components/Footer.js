3bbd9f0d4e5d2c3376043d28b5ff37a278c467dc
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2020-12-09 19:56:58 -0500

    Styled footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,25 +4,23 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className="footer">
-            <div>
-                <div className="footerListContainer">
-                    <div>
-                    <h3 className="footerListHeader">Panayiotis' Links</h3>
-                    <ul className="footerList">
-                        <li><a href="https://www.linkedin.com/in/panayiotis-dimopoulos/">LinkedIn</a></li>
-                        <li><a href="https://github.com/PanosPHL">GitHub</a></li>
-                        <li><a href="https://panosphl.github.io/panos-portfolio/">Portfolio</a></li>
-                    </ul>
+                    <div className="footerListContainer">
+                        <h3 className="footerListHeader">Panayiotis' Links</h3>
+                        <ul className="footerList">
+                            <li><a className="footerListItem" href="https://www.linkedin.com/in/panayiotis-dimopoulos/">LinkedIn</a></li>
+                            <li><a className="footerListItem" href="https://github.com/PanosPHL">GitHub</a></li>
+                            <li><a className="footerListItem" href="https://panosphl.github.io/panos-portfolio/">Portfolio</a></li>
+                        </ul>
                     </div>
-                    <div>
-                        <h3>Panayiotis' Project Links</h3>
-                        <li><a href="https://github.com/PanosPHL/PlayerSeekingPlayer">Player Seeking Player Repo</a></li>
-                        <li><a href="https://flownotes.herokuapp.com">flowNotes</a></li>
-                        <li><a href="https://everquote.herokuapp.com">EverQuote</a></li>
-                        <li><a href="https//aagoodreads.herokuapp.com">aAGoodreads</a></li>
+                    <div className="footerListContainer">
+                        <h3 className="footerListHeader">Panayiotis' Project Links</h3>
+                        <ul className="footerList">
+                            <li><a className="footerListItem" href="https://github.com/PanosPHL/PlayerSeekingPlayer">Player Seeking Player Repo</a></li>
+                            <li><a className="footerListItem" href="https://flownotes.herokuapp.com">flowNotes</a></li>
+                            <li><a className="footerListItem" href="https://everquote.herokuapp.com">EverQuote</a></li>
+                            <li><a className="footerListItem" href="https//aagoodreads.herokuapp.com">aAGoodreads</a></li>
+                        </ul>
                     </div>
-                </div>
-            </div>
         </div>
     )
 }
