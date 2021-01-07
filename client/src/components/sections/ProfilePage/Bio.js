0b35573e1ef5c3cf89c7f6edb5dc6c8f2ee86e29
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-07 18:33:31 -0500

    Refactor of about section

--- client/src/components/sections/ProfilePage/Bio.js
+++ client/src/components/sections/ProfilePage/Bio.js
@@ -6,27 +6,29 @@ import BioEditButton from '../../buttons/BioEditButton';
 import aboutStyles from '../../../css-modules/About.module.css';
 
 const Bio = ({ userProfile, isOwner }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleEditClick = () => {
-        dispatch(toggleBioModal());
-    }
+  const handleEditClick = () => {
+    dispatch(toggleBioModal());
+  };
 
-    const value = {
-        handleEditClick
-    }
+  const value = {
+    handleEditClick,
+  };
 
-    return (
-        <BioContext.Provider value={value}>
-        <div className={aboutStyles.aboutSectionInnerContainer}>
-            <BioEditButton isOwner={isOwner}/>
-            <div className={aboutStyles.bio}>
-                <h3 className={aboutStyles.aboutSectionHeader}>Biography</h3>
-                <div className={aboutStyles.bioContent}>{userProfile ? userProfile.profileInfo.biography.split('\n').map((line) => <p>{line}</p>) : ""}</div>
-            </div>
-        </div>
-        </BioContext.Provider>
-    )
-}
+  return (
+    <BioContext.Provider value={value}>
+      <BioEditButton isOwner={isOwner} />
+      <h3 className={aboutStyles.aboutSectionHeader}>Biography</h3>
+      <div className={aboutStyles.bioContent}>
+        {userProfile
+          ? userProfile.profileInfo.biography
+              .split('\n')
+              .map((line) => <p>{line}</p>)
+          : ''}
+      </div>
+    </BioContext.Provider>
+  );
+};
 
 export default Bio;
