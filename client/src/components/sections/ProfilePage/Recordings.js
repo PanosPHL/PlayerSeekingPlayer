8c2f8ca590f4dbb5514f7d8411f467ff025217d2
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-08 10:18:33 -0500

    Styled recordings section

--- client/src/components/sections/ProfilePage/Recordings.js
+++ client/src/components/sections/ProfilePage/Recordings.js
@@ -6,52 +6,68 @@ import AddRecordingButton from '../../buttons/AddRecordingButton';
 import RecordingFormContext from '../../../contexts/RecordingFormContext';
 import recordingStyles from '../../../css-modules/Recordings.module.css';
 
-
 const Recordings = ({ isOwner, userProfile, userRecordings, className }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const handleNewRecordingClick = () => {
-        dispatch(toggleRecordingModal());
-    }
+  const handleNewRecordingClick = () => {
+    dispatch(toggleRecordingModal());
+  };
 
-    const value = {
-        handleNewRecordingClick
-    };
+  const value = {
+    handleNewRecordingClick,
+  };
 
-    return (
-        <RecordingFormContext.Provider value={value}>
-        <div className={recordingStyles.recordingsContainer}>
-            <div className={recordingStyles.recordingsHeaderContainer}>
-                <h2 className={recordingStyles.recordingsHeader}>Recordings</h2>
-            <AddRecordingButton isOwner={isOwner} />
-            </div>
-            <div style={{
-            display: "grid",
-            gridTemplateRows: userRecordings ? `repeat(${userRecordings.length}, 1fr)` : ""}}>
-            {
-            userRecordings.length ?
-            userRecordings.map((recording, i) => <Recording key={`recording-${i + 1}`} recording={recording} i={i + 1} isOwner={isOwner}/>) :
-            <div className={recordingStyles.recordingPlaceholderContainer}>
-                <h1 className={recordingStyles.recordingPlaceholderText}>This user has not yet added any recordings</h1>
-            </div>
-            }
-            </div>
+  return (
+    <RecordingFormContext.Provider value={value}>
+      <div className={recordingStyles.recordingsContainer}>
+        <div className={recordingStyles.recordingsHeaderContainer}>
+          <h2 className={recordingStyles.recordingsHeader}>Recordings</h2>
+          <AddRecordingButton isOwner={isOwner} />
         </div>
-        </RecordingFormContext.Provider>
-    )
-}
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+          }}
+        >
+          {userRecordings.length ? (
+            userRecordings.map((recording, i) => (
+              <Recording
+                key={`recording-${i + 1}`}
+                recording={recording}
+                i={i + 1}
+                isOwner={isOwner}
+              />
+            ))
+          ) : (
+            <div className={recordingStyles.recordingPlaceholderContainer}>
+              <h1 className={recordingStyles.recordingPlaceholderText}>
+                This user has not yet added any recordings
+              </h1>
+            </div>
+          )}
+        </div>
+      </div>
+    </RecordingFormContext.Provider>
+  );
+};
 
 const mapStateToProps = (state, { isOwner, userProfile }) => {
-    return {
-        userRecordings: userProfile && Object.keys(state.entities.recordings).length ? Object.values(userProfile.profileInfo.recordings).map((recording) => {
+  return {
+    userRecordings:
+      userProfile && Object.keys(state.entities.recordings).length
+        ? Object.values(userProfile.profileInfo.recordings).map((recording) => {
             return {
-                id: recording.recording_id,
-                url: state.entities.recordings[recording.recording_id] ? state.entities.recordings[recording.recording_id].url : null,
-                title: recording.title,
-                description: recording.description
-            }
-        }) : []
-    }
-}
+              id: recording.recording_id,
+              url: state.entities.recordings[recording.recording_id]
+                ? state.entities.recordings[recording.recording_id].url
+                : null,
+              title: recording.title,
+              description: recording.description,
+            };
+          })
+        : [],
+  };
+};
 
 export default connect(mapStateToProps)(Recordings);
