ef9ea7b6e225ae1e4f520a0135af1922134fbcaf
Panayiotis Dimopoulos <panayiotis.dimopoulos@gmail.com>
2021-01-08 20:09:19 -0500

    Styled notification number

--- client/src/components/dropdowns/ProfilePicAndDropdown.js
+++ client/src/components/dropdowns/ProfilePicAndDropdown.js
@@ -5,22 +5,53 @@ import NavContext from '../../contexts/NavContext';
 import navStyles from '../../css-modules/NavComponents.module.css';
 
 const ProfilePicAndDropdown = ({ profileDropdown, notifications }) => {
-    const { handleProfileDropdownClick } = useContext(NavContext);
-    const user = useSelector(state => state.entities.users[state.session.userId]);
+  const { handleProfileDropdownClick } = useContext(NavContext);
+  const user = useSelector(
+    (state) => state.entities.users[state.session.userId]
+  );
 
-    return (
-        <div className={navStyles.profileContainer} onClick={handleProfileDropdownClick}>
-            <div styles={{ position: "relative" }}>
-            <img className={navStyles.profileImage} src={user ? user.profileInfo.profilePic || '/static/images/default_profile_photo.jpg' : ''} alt='Profile Placeholder' />
-            {
-                notifications ? <span className={navStyles.picNotificationNumber}>{notifications}</span>
-                : <></>
-            }
-            </div>
-            <span className={navStyles.profileDropdownArrow + " " + (profileDropdown ? navStyles.active : navStyles.inactive)}>⯆</span>
-            <ProfileDropdown notifications={notifications} userId={user ? user.id : null} className={profileDropdown ? navStyles.dropdownContainer : navStyles.hidden} />
-        </div>
-    )
-}
+  return (
+    <div
+      className={navStyles.profileContainer}
+      onClick={handleProfileDropdownClick}
+    >
+      <div style={{ position: 'relative' }}>
+        <img
+          className={navStyles.profileImage}
+          src={
+            user
+              ? user.profileInfo.profilePic ||
+                '/static/images/default_profile_photo.jpg'
+              : ''
+          }
+          alt="Profile Placeholder"
+        />
+        {notifications ? (
+          <span className={navStyles.picNotificationNumber}>
+            {notifications}
+          </span>
+        ) : (
+          <></>
+        )}
+      </div>
+      <span
+        className={
+          navStyles.profileDropdownArrow +
+          ' ' +
+          (profileDropdown ? navStyles.active : navStyles.inactive)
+        }
+      >
+        ⯆
+      </span>
+      <ProfileDropdown
+        notifications={notifications}
+        userId={user ? user.id : null}
+        className={
+          profileDropdown ? navStyles.dropdownContainer : navStyles.hidden
+        }
+      />
+    </div>
+  );
+};
 
 export default ProfilePicAndDropdown;
